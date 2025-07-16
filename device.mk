c6c1322ba65f0abe3f2fecde85a52a90b5510c18
Koushik Dey <kousickdey3@gmail.com>
2025-07-17 02:29:36 +0530

    RMX2020: Move config_cleanupUnusedFingerprints bool into lineage-sdk * Fixes fingerprint cleanups on every boot
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Change-Id: Icb80104d66f4faa02e504f59183746ab192bf875

--- device.mk
+++ device.mk
@@ -195,6 +195,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/nfc/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := $(DEVICE_PACKAGE_OVERLAYS)
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay-lineage
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
